73227fe3dab716eaf3e9812be0872ce320ddfd7e
Ffej Caplan <ffej.caplan@gmail.com>
2020-06-16 15:03:01 -0400

    updated PORT for heroku push

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 
 app.use(express.static("db/public"));
 
